fb117e8c81a8d68034510a112805330529ac5f40
Vitaliy Minayev <Vitaliy_Minayev@epam.com>
2021-02-07 16:36:31 +0200

    update calculation function

--- SpCalculatorUnit.pas
+++ SpCalculatorUnit.pas
@@ -1,11 +1,18 @@
 unit SpCalculatorUnit;
 
 interface
+const
+   BATT_VOLTAGE = 12;
+   BATT_DEF_CAPACITY = 50;
+   BATT_EFF_RATIO = 0.9;
+   SOLAR_PANEL_POWER = 250;
+   SOLAR_PANEL_SQUARE = 2;
 
 type
    TCalculationResults = record
       PanelsAmount : byte;
       BatteriesAmount : byte;
+      RoofSpaceNeeded : byte;
    end;
 
 function CalculateBatteriesPanelsAmount(SHoursAmount:Word; ConsumedElectricity:Word): TCalculationResults;
@@ -15,15 +22,10 @@ implementation
 
 
 function CalculateBatteriesPanelsAmount(SHoursAmount:Word; ConsumedElectricity:Word): TCalculationResults;
-var
-   t:integer;
 begin
-
-   t := SHoursAmount + ConsumedElectricity;
-
-   Result.PanelsAmount := 1;
-   Result.BatteriesAmount := 2;
-
+   Result.BatteriesAmount := Round(ConsumedElectricity/(BATT_VOLTAGE*BATT_DEF_CAPACITY*BATT_EFF_RATIO));
+   Result.PanelsAmount := Round(ConsumedElectricity/(SHoursAmount*SOLAR_PANEL_POWER));
+   Result.RoofSpaceNeeded := Result.PanelsAmount*SOLAR_PANEL_SQUARE;
 end;
 
 end.
